0fbbcd7d1d6f919a7db3426b727426327aa3785d
Rogier van Staveren <rogier@batoid.com>
2024-04-23 07:06:44 +0200

    Expose Section classes

--- unii/__init__.py
+++ unii/__init__.py
@@ -6,4 +6,9 @@ __version__ = "0.0.0.1"
 
 from .sia_code import SIACode
 from .unii import DEFAULT_PORT, UNii, UNiiCommand, UNiiData, UNiiLocal
-from .unii_command_data import UNiiInputState, UNiiInputStatusRecord
+from .unii_command_data import (
+    UNiiInputState,
+    UNiiInputStatusRecord,
+    UNiiSection,
+    UNiiSectionArmedState,
+)
