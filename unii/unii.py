55588fe2928a39ec43a47b6ffc97dda5abca084a
Rogier van Staveren <rogier@batoid.com>
2024-05-15 12:47:06 +0200

    Suppress unkown section warning

--- unii/unii.py
+++ unii/unii.py
@@ -29,6 +29,7 @@ from .unii_command_data import (
     UNiiOutput,
     UNiiRawData,
     UNiiSection,
+    UNiiSectionArmedState,
     UNiiSectionArrangement,
     UNiiSectionStatus,
 )
@@ -339,7 +340,7 @@ class UNiiLocal(UNii):
         for _, section_status in data.items():
             if section_status.number in self.sections:
                 self.sections[section_status.number].update(section_status)
-            else:
+            elif section_status.armed_state != UNiiSectionArmedState.NOT_PROGRAMMED:
                 # This should never happen
                 logger.warning(
                     "Status for unknown section %i changed", section_status.number
