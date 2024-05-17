3391be3671efc9de83d9ab869918a3a9088015f0
Rogier van Staveren <rogier@batoid.com>
2024-05-17 04:23:39 +0200

    black formatting

--- unii/unii.py
+++ unii/unii.py
@@ -348,7 +348,9 @@ class UNiiLocal(UNii):
         for _, section_status in data.items():
             if section_status.number in self.sections:
                 self.sections[section_status.number].update(section_status)
-                self.sections[section_status.number]["active"] = section_status.armed_state != UNiiSectionArmedState.NOT_PROGRAMMED
+                self.sections[section_status.number]["active"] = (
+                    section_status.armed_state != UNiiSectionArmedState.NOT_PROGRAMMED
+                )
             elif section_status.armed_state != UNiiSectionArmedState.NOT_PROGRAMMED:
                 # This should never happen
                 logger.warning(
