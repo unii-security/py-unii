48906af7f091373e30e017df2155a788e0c85c8a
Rogier van Staveren <rogier@batoid.com>
2024-05-23 19:22:57 +0200

    Enrich section status before section update

--- unii/unii.py
+++ unii/unii.py
@@ -347,10 +347,10 @@ class UNiiLocal(UNii):
     def _handle_section_status(self, data: UNiiSectionStatus):
         for _, section_status in data.items():
             if section_status.number in self.sections:
-                self.sections[section_status.number].update(section_status)
-                self.sections[section_status.number]["active"] = (
+                section_status["active"] = (
                     section_status.armed_state != UNiiSectionArmedState.NOT_PROGRAMMED
                 )
+                self.sections[section_status.number].update(section_status)
             elif section_status.armed_state != UNiiSectionArmedState.NOT_PROGRAMMED:
                 # This should never happen
                 logger.warning(
